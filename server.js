0b210df6a439cc3b41b536d5b534edfa1340cb17
Nicholas Bering <bering.nicholas@gmail.com>
2020-10-10 17:38:51 -0400

    Basic VLC client to fetch artist and title fields

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = process.env["PORT"] || 3000;
 
+const VLC = require("./lib/vlc");
+const vlcClient = new VLC(null, process.env["VLC_PASS"]);
+
 app.engine("handlebars", expressHandlebars());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    vlcClient.getStatus()
+        .then(status => {
+            res.render("index", {
+                vlcStatus: status
+            });
+        });
 });
 
 app.listen(port, () =>{
