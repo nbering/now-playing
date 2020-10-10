05b6c35c16b1b9952f66eb6d8345786354044edd
Nicholas Bering <bering.nicholas@gmail.com>
2020-10-10 13:56:01 -0400

    View engine boilerplate

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 
 const app = express();
 const port = process.env["PORT"] || 3000;
+
+app.engine("handlebars", expressHandlebars());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
 app.listen(port, () =>{
     console.log(`Now Playing server listening at http://localhost:${port}`)
